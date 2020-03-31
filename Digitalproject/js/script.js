9eb7760c46ef66329dc00bfd333f6b7f9c0c2a46
hidri <agthidri2400@gmail.com>
2020-03-31 19:25:55 +0300

    this commit will add the all effect of animation

--- Digitalproject/js/script.js
+++ Digitalproject/js/script.js
@@ -2,5 +2,70 @@
 
 $(window).on('load',function(){
     $('#status').fadeOut();
-    $('#preloader').delay(400).fadeOut('slow');
+    $('#preloader').delay(300).fadeOut('slow');
 });
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        autoplay:true,
+        smartSpeed:500,
+        loop:true,
+        autoplayHoverPause:true,
+        nav:true,
+        dots:false 
+    });
+  });
+
+  /* progress bar */
+
+  $(function(){
+      $(".progress-bar").each(function(){
+          $(this).animate({
+              width: $(this).attr("aria-valuenow") + "%"}
+              ,1000);
+      });
+  });
+
+  function validation(){
+      var name = document.getElementById("name").value;
+      var subject = document.getElementById("subject").value;
+      var phone = document.getElementById("phone").value;
+      var message = document.getElementById("message").value;
+      var email = document.getElementById("email").value;
+      var errorm = document.getElementById("errorm");
+      var text;
+
+     errorm.style.padding = "10px";
+
+     if (name.length < 4){
+         text = "please Enter Valid Name";
+         errorm.innerHTML = text;
+         return false ;
+     }
+     if (subject.length < 10){
+        text = "please Enter Valid subjectName";
+        errorm.innerHTML = text;
+        return false ;
+
+  }
+  if (email.indexOf("@") == -1 || email.length < 6){
+    text = "please Enter Valid email";
+    errorm.innerHTML = text;
+    return false ;
+
+}
+alert("form sunmitted success")
+  }
+
+  /* animation*/
+
+  $ (function(){
+    new WOW().init();
+  });
+  $(window).on('load', function(){
+        $("#home-heading-1").addClass("animated fadeInDown");
+        $("#home-heading-2").addClass("animated fadeInLeft");
+        $("#home-text").addClass("animated zoomIn");
+        $("#arrow-down i").addClass("animated fadeInDown infinite");
+  });
